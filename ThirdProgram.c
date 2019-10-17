ac71dc91f86ec4df6009f6cbb38074d58b6e825f
Imran Ali <imran.cs.uob@gmail.com>
2019-10-17 15:16:47 +0500

    using format specifiers with specific width

---
+++ ThirdProgram.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(void){
+
+ float numb_one, numb_two, result;
+ numb_one=12;
+ numb_two=26;
+
+ printf("\n%f + %f = %f", numb_one, numb_two, numb_one + numb_two);
+ printf("\n%f - %f = %f", numb_one, numb_two, numb_one - numb_two);
+ printf("\n%f * %f = %f", numb_one, numb_two, numb_one * numb_two);
+ printf("\n%f / %f = %.2f", numb_two, numb_one, numb_two / numb_one);
+
+ return 0;
+}
